4560b982c5d277e00a2560c1df05b35aab6085ad
Brenotcs <brenotcs@gmail.com>
2026-03-03 22:38:14 -0300

    imagens importadas

--- project/src/data/beforeAfterImages.ts
+++ project/src/data/beforeAfterImages.ts
@@ -1,18 +1,25 @@
 
+import editantes1 from '../assets/antesedepois/editantes1.png';
+import editdepois1 from '../assets/antesedepois/editdepois1.png';
+import editantes2 from '../assets/antesedepois/editantes2.png';
+import editdepois2 from '../assets/antesedepois/editdepois2.jpg';
+import editantes3 from '../assets/antesedepois/editantes3.jpg';
+import editdepois3 from '../assets/antesedepois/editdepois3.jpg';
+
 export const beforeAfterImages = [
   {
-    before: '/src/assets/antesedepois/editantes1.png',
-    after: '/src/assets/antesedepois/editdepois1.png',
+    before: editantes1,
+    after: editdepois1,
     resultado: 'Onicomicose (Ainda em Tratamento)'
   },
   {
-    before: '/src/assets/antesedepois/editantes2.png',
-    after: '/src/assets/antesedepois/editdepois2.jpg',
+    before: editantes2,
+    after: editdepois2,
     resultado: 'Onicomicose - Pé esquerdo'
   },
   {
-    before: '/src/assets/antesedepois/editantes3.jpg',
-    after: '/src/assets/antesedepois/editdepois3.jpg',
+    before: editantes3,
+    after: editdepois3,
     resultado: 'Micose (Unha da Mão)'
   }
 ];
